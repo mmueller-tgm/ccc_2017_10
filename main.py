42a78511b695e37d39069b080255bd3e92caf9d2
mmuellerTGM <16237764+mmueller-tgm@users.noreply.github.com>
2017-10-20 13:54:29 +0200

    a bit less initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.5
+
+class main:
+    def __init__(self, file):
+        self.file = file
+
+    def reader(self):
+        pass
+
+
+if __name__ == "__main__":
+    main("L1/level1_1.txt")
+    main("L1/level1_1.txt")
+    main("L1/level1_1.txt")
+    main("L1/level1_1.txt")
